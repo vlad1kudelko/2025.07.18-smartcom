27a87e690e4772d885f1666281892115eea3ac20
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-07-22 00:15:22 +0800

    -

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-cd base         && docker compose down && docker compose up --build -d && cd ..
-cd minio        && docker compose down && docker compose up --build -d && cd ..
+cd base           && docker compose down && docker compose up --build -d && cd ..
+cd minio          && docker compose down && docker compose up --build -d && cd ..
 
-cd mod_admin    && docker compose down && docker compose up --build -d && cd ..
-cd mod_watchdog && docker compose down && docker compose up --build -d && cd ..
+cd mod_admin      && docker compose down && docker compose up --build -d && cd ..
+cd mod_downloader && docker compose down && docker compose up --build -d && cd ..
+cd mod_watchdog   && docker compose down && docker compose up --build -d && cd ..
 
-cd rabbitmq     && docker compose down && docker compose up --build -d && cd ..
-cd sftp_servers && docker compose down && docker compose up --build -d && cd ..
+cd rabbitmq       && docker compose down && docker compose up --build -d && cd ..
+cd sftp_servers   && docker compose down && docker compose up --build -d && cd ..
--- stop.sh
+++ stop.sh
@@ -1,8 +1,9 @@
-cd base         && docker compose down && cd ..
-cd minio        && docker compose down && cd ..
+cd base           && docker compose down && cd ..
+cd minio          && docker compose down && cd ..
 
-cd mod_admin    && docker compose down && cd ..
-cd mod_watchdog && docker compose down && cd ..
+cd mod_admin      && docker compose down && cd ..
+cd mod_downloader && docker compose down && cd ..
+cd mod_watchdog   && docker compose down && cd ..
 
-cd rabbitmq     && docker compose down && cd ..
-cd sftp_servers && docker compose down && cd ..
+cd rabbitmq       && docker compose down && cd ..
+cd sftp_servers   && docker compose down && cd ..

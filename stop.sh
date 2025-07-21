a6ee22d0e55ef3d8047f7ab6ee28cf7243cbd6c6
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-07-21 21:53:38 +0800

    stop

---
+++ stop.sh
@@ -0,0 +1,8 @@
+cd base         && docker compose down && cd ..
+cd minio        && docker compose down && cd ..
+
+cd mod_admin    && docker compose down && cd ..
+cd mod_watchdog && docker compose down && cd ..
+
+cd rabbitmq     && docker compose down && cd ..
+cd sftp_servers && docker compose down && cd ..
